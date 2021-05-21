28bacc78b026926c35f7192206db6f490d7d49da
andresmellimaci <andresmelli90@hotmail.com>
2021-05-21 09:57:40 -0300

    Se agrega el componente App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ListaProductos } from "./components/ListaProductos.js";
+
+function App() {
+  return (
+    <div>
+    <ListaProductos />
+    </div>
+  );
+}
+
+export default App;
