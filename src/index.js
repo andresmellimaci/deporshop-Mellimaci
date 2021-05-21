a2e0dfc58e00a4174d2af01ca48d1c171e3f2ec4
andresmellimaci <andresmelli90@hotmail.com>
2021-05-21 11:31:58 -0300

    Correccion en el archivo index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { ListaProductos } from './components/ListaProductos';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ListaProductos></ListaProductos>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
